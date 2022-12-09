f290b6a8711826731fac25b5bec552d78f16643f
vinishred <callarecvincent@gmail.com>
2022-12-09 11:37:50 +0000

    new code

---
+++ les conditions.php
@@ -0,0 +1,16 @@
+<?php
+$pays = 'Espagne';
+if ($pays === 'Allemagne') {
+  echo 'Hallo';
+} else if ($pays === 'Angleterre') {
+  echo 'Hello';
+} else if ($pays === 'Espagne') {
+  echo 'Buenos dias';
+} else if ($pays === 'France') {
+  echo 'Bonjour';
+} else if ($pays === 'Italie') {
+  echo 'Buongiorno';
+} else {
+  echo 'Bonjour';
+}
+?>
