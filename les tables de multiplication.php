e3ab9289cde8c197f5aaed1522ec22bb304c46b0
vinishred <callarecvincent@gmail.com>
2022-12-12 11:53:06 +0000

    utilisation de la boucle for

---
+++ les tables de multiplication.php
@@ -0,0 +1,13 @@
+<?php
+
+for ($number = 1; $number<= 10; $number++) 
+{
+  for ($multiplicate = 1; $multiplicate <=10; $multiplicate++) 
+  {
+    echo $number. ' fois ' .$multiplicate. ' = ' .($number * $multiplicate). '<br>';
+    
+  }
+  echo '<br>';
+}
+  
+
